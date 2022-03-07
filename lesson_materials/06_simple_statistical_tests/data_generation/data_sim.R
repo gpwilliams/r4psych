0b1015e450bf756d23467d42eb951e6ec42b8938
Glenn Patrick Williams <33417082+gpwilliams@users.noreply.github.com>
2022-03-07 11:24:40 +0000

    Updated files to new bookdown

--- lesson_materials/06_simple_statistical_tests/data_generation/data_sim.R
+++ lesson_materials/06_simple_statistical_tests/data_generation/data_sim.R
@@ -2,7 +2,7 @@
 # Simulate Data
 #
 # Generates data sets for analysis with t-tests, ANOVA, and correlation
-#  
+#
 ###############################################################################
 
 # load library; download devtools and funfact if not already installed
@@ -15,24 +15,27 @@ library(tidyverse)
 ########################
 
 # specify design
-wsubj_design <- list(ivs = c(A = 2),
-                  n_item = 40
-                  )
+wsubj_design <- list(
+  ivs = c(A = 2),
+  n_item = 40
+)
 
 # specify parameters
-beta <- list("(Intercept)" = c(300, 350), 
-             "AA2" = c(100, 120)
-             )
+beta <- list(
+  "(Intercept)" = c(300, 350),
+  "AA2" = c(100, 120)
+)
+
 theta <- c(25, 50)
 sigma <- c(25, 30)
 
 # specify poplation parameters
-popdata <- gen_pop(wsubj_design, 
-                   n_subj = 60, 
-                   fixed_ranges = beta, 
-                   var_range = theta, 
-                   err_range = sigma
-                   )
+popdata <- gen_pop(wsubj_design,
+  n_subj = 60,
+  fixed_ranges = beta,
+  var_range = theta,
+  err_range = sigma
+)
 
 # simulate data
 set.seed(1000)
@@ -45,9 +48,10 @@ wsubj_data %>% group_by(A) %>% summarise(m = mean(Y), sd = sd(Y))
 #########################
 
 # specify design
-bsubj_design <- list(ivs = c(A = 2),
-                  n_item = 40,
-                  between_subj = "A"
+bsubj_design <- list(
+  ivs = c(A = 2),
+  n_item = 40,
+  between_subj = "A"
 )
 
 # simulate data
@@ -63,18 +67,22 @@ bsubj_data %>% group_by(A) %>% summarise(m = mean(Y), sd = sd(Y))
 corr_data <- tibble(
   subject = seq(1:60),
   sex = c(rep("male", 30), rep("female", 30)),
-  weight = c(rnorm(n = 30, mean = 84, sd = 7.5), rnorm(n = 30, mean = 69, sd = 7.5)),
+  weight = c(
+    rnorm(n = 30, mean = 84, sd = 7.5),
+    rnorm(n = 30, mean = 69, sd = 7.5)
+    ),
   height = c(weight + rnorm(n = 60, mean = 90, sd = 2))
-  )
+)
 
 # check data
-corr_data %>% 
-  group_by(sex) %>% 
-  summarise(m_weight = mean(weight),
-            m_weight = mean(height),
-            sd = sd(weight),
-            sd = sd(height)
-            )
+corr_data %>%
+  group_by(sex) %>%
+  summarise(
+    m_weight = mean(weight),
+    m_weight = mean(height),
+    sd = sd(weight),
+    sd = sd(height)
+  )
 
 # binomial between subjects
 ###########################
@@ -94,38 +102,40 @@ binom_between_data <- tibble(
 ###########################
 
 subject <- seq(1:100)
-prob = 0.3
-n = 100
+prob <- 0.3
+n <- 100
 
 set.seed(1000)
 binom_within_data <- tibble(
   subject = subject,
   pre_test = rbinom(n = n, size = 1, prob = prob),
   post_test = rbinom(n = n, size = 1, prob = prob)
-  )
+)
 
 # make all successes in post if success in pre
 binom_within_data <- binom_within_data %>%
-  mutate(post_test = 
-           case_when(pre_test == 1 ~ 1, 
-                     TRUE ~ as.double(post_test)
-                     ),
-         post_test = as.integer(post_test)
-         )
+  mutate(
+    post_test =
+      case_when(
+        pre_test == 1 ~ 1,
+        TRUE ~ as.double(post_test)
+      ),
+    post_test = as.integer(post_test)
+  )
 
 # save all data
 ###############
 
 save_list <- list(
-  bsubj_data = bsubj_data, 
+  bsubj_data = bsubj_data,
   wsubj_data = wsubj_data,
   corr_data = corr_data,
   binom_between_data = binom_between_data,
   binom_within_data = binom_within_data
-  )
+)
 
-mapply(write.csv, 
-       save_list, 
-       file = paste0("../inputs/", names(save_list), ".csv"), 
-       row.names = F
-       )
+mapply(write.csv,
+  save_list,
+  file = paste0("../inputs/", names(save_list), ".csv"),
+  row.names = F
+)
